71d845d5662520fd71eb6f6f08e96da93543f56a
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-11-21 16:58:12 +0300

    update(UserController): writing to db

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -2,8 +2,20 @@ package com.hoaxify.ws.user;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String username;
 
     private String displayName;
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,18 +1,20 @@
 package com.hoaxify.ws.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 
 
 @RestController
 public class UserController {
 
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/api/v1/users")
     public String sayHi(){
@@ -21,6 +23,6 @@ public class UserController {
 
     @PostMapping("/api/v1/users")
     public void createUser(@RequestBody User user){
-            log.info(user.toString());
+            userRepository.save(user);
     }
 }
