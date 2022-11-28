35e46502b21fe796d9d60f791960c90d734502c2
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-11-28 09:37:01 +0300

    add (Login) : create user while app starting

--- src/main/java/com/hoaxify/ws/WsApplication.java
+++ src/main/java/com/hoaxify/ws/WsApplication.java
@@ -1,14 +1,32 @@
 package com.hoaxify.ws;
 
+import com.hoaxify.ws.user.User;
+import com.hoaxify.ws.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner createInitialUser(UserService userService){
+		return new CommandLineRunner(){
+			@Override
+			public void run(String... args) throws Exception {
+				User user =new User();
+				user.setUsername("user1");
+				user.setPassword("Password1");
+				user.setDisplayName("disname1");
+				userService.save(user);
+			}
+		};
 	}
 
 }
